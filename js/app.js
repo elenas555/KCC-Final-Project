0c8529aa707241e2c96f35bcbc0f4cad49643b4b
Elena Sanilevich <elena555@me.com>
2018-06-20 19:17:37 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// $(document).ready(function() {
+// $("#dropdown").hover(function(){
+//   $(this).show();
+// });
+//
+//
+// });
+
+
+// $("p").click(function(){
+//         $(this).hide();
+//     });
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+var isDisabled = false;
+function nextSlide() {
+  if (isDisabled) {
+    return;
+  }
+
+  showSlides(slideIndex += 1);
+  setTimeout(nextSlide, 5000);
+};
+
+nextSlide();
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
